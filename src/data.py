342838fdfa34afb8bf0b72553b1467b7d01ea648
Ubuntu <ubuntu@ip-172-31-49-126.ec2.internal>
2021-01-26 13:52:22 +0000

    Data Pipeline

--- src/data.py
+++ src/data.py
@@ -1,13 +1,13 @@
 import os
 import numpy as np
-import pandas as pd
+# import pandas as pd
 import tensorflow as tf
 
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
+# os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+# tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
 
 '''
-Create tf.data.Dataset from TFRecords from a directory of TFRecords.
+Create tf.data.Dataset from a directory of Images.
 Preprocess Images : Resize, Flip, Normalize
 '''
 
@@ -20,23 +20,34 @@ def preprocess_image(image, image_size):
     return image
 
 
-def parse_image_function(example_proto, image_size):
-    
-    image_feature_description = {
-        'image_shape': tf.io.FixedLenFeature(shape=[3, ], dtype=tf.int64),
-        'id': tf.io.FixedLenFeature(shape=[], dtype=tf.int64),
-        'image_raw': tf.io.FixedLenFeature(shape=[], dtype=tf.string),
-    }
-    
-    parsed_example = tf.io.parse_single_example(example_proto, image_feature_description)
-    image_string = parsed_example['image_raw']
+def parse_image_function(image, image_size):
+      
+    image_string = tf.read_file(image)
     image = tf.image.decode_jpeg(image_string, channels=3)
     image = preprocess_image(image, image_size)
-    label = parsed_example['id']
+    label = image.split('/')[-2]
     return image, label
 
 
-def get_dataset(tfrcd_dir, params, phase='train'):
+def get_dataset(dir, params=5, phase='train'):
+
+    dir_paths  =  os.listdir(dir)
+    dir_paths  =  [os.path.join(dir, dir_path) for dir_path in dir_paths]
+
+    image_paths = []
+    for dir_path in dir_paths:
+        for image_path in os.listdir(dir_path):
+            image_paths.append(os.path.join(dir_path, image_path))
+
+    AUTOTUNE   =  tf.data.experimental.AUTOTUNE
+    dataset    =  tf.data.Dataset.from_tensor_slices(file_paths)
+    dataset    =  dataset.map(lambda x: parse_image_function(x, 250)) #change here
+    dataset    =  dataset.batch(24).prefetch(AUTOTUNE) #change here
+    
+    return dataset, len(file_paths)
+
+
+def old_get_dataset(tfrcd_dir, params, phase='train'):
     
     if phase == 'train':
         df = pd.read_csv(os.path.join(tfrcd_dir,'info.csv'))
@@ -54,3 +65,8 @@ def get_dataset(tfrcd_dir, params, phase='train'):
     dataset    =  dataset.batch(params.batch_size).prefetch(AUTOTUNE)
     
     return dataset, nrof_samples
+
+
+if __name__ == "__main__":
+
+    get_dataset('~/face-data')
