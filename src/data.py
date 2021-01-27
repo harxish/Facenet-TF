0403371ac8c15217399ea5a7545f9f06484afde3
Ubuntu <ubuntu@ip-172-31-93-199.ec2.internal>
2021-01-27 06:08:38 +0000

    IDK

--- src/data.py
+++ src/data.py
@@ -20,53 +20,34 @@ def preprocess_image(image, image_size):
     return image
 
 
-def parse_image_function(image, image_size):
+def parse_image_function(image_path, label, image_size):
       
-    image_string = tf.read_file(image)
+    image_string = tf.io.read_file(image_path)
     image = tf.image.decode_jpeg(image_string, channels=3)
     image = preprocess_image(image, image_size)
-    label = image.split('/')[-2]
     return image, label
 
 
-def get_dataset(dir, params=5, phase='train'):
+def get_dataset(dir, params, phase='train'):
 
     dir_paths  =  os.listdir(dir)
     dir_paths  =  [os.path.join(dir, dir_path) for dir_path in dir_paths]
 
     image_paths = []
+    image_label = []
     for dir_path in dir_paths:
         for image_path in os.listdir(dir_path):
             image_paths.append(os.path.join(dir_path, image_path))
+            image_label.append(int(dir_path.split('/')[-1][1:]))
 
     AUTOTUNE   =  tf.data.experimental.AUTOTUNE
-    dataset    =  tf.data.Dataset.from_tensor_slices(file_paths)
-    dataset    =  dataset.map(lambda x: parse_image_function(x, 250)) #change here
-    dataset    =  dataset.batch(24).prefetch(AUTOTUNE) #change here
-    
-    return dataset, len(file_paths)
-
-
-def old_get_dataset(tfrcd_dir, params, phase='train'):
-    
-    if phase == 'train':
-        df = pd.read_csv(os.path.join(tfrcd_dir,'info.csv'))
-        nrof_samples = df['train_num'][0]
-        
-    elif phase == 'val':
-        df = pd.read_csv(os.path.join(tfrcd_dir,'info.csv'))
-        nrof_samples = df['val_num'][0]
-      
-    file_paths =  os.listdir(os.path.join(tfrcd_dir, phase))
-    file_paths =  [os.path.join(tfrcd_dir, phase, file_path)for file_path in file_paths]
-    AUTOTUNE   =  tf.data.experimental.AUTOTUNE
-    dataset    =  tf.data.TFRecordDataset(file_paths)
-    dataset    =  dataset.map(lambda x: parse_image_function(x, params.image_size))
+    dataset    =  tf.data.Dataset.from_tensor_slices((image_paths, image_label))
+    dataset    =  dataset.map(lambda x, y: parse_image_function(x, y, params.image_size))
     dataset    =  dataset.batch(params.batch_size).prefetch(AUTOTUNE)
     
-    return dataset, nrof_samples
+    return dataset, len(image_paths)
 
 
 if __name__ == "__main__":
 
-    get_dataset('~/face-data')
+    print(get_dataset('../face-data'))
